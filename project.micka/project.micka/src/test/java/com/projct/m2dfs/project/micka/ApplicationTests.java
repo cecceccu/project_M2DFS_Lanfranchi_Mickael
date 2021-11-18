77d80d421d2148fa587cea6c6d7534c84910cd8a
Mickael Lanfranchi <58193062+cecceccu@users.noreply.github.com>
2021-11-18 13:55:22 +0100

    first microservice created

---
+++ project.micka/project.micka/src/test/java/com/projct/m2dfs/project/micka/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projct.m2dfs.project.micka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
